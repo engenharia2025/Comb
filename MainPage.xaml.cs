cd17bf9c332aa3e94e02b27e9390195351ec9711
kelvin.souza <kelvin.souza@TI370-PC>
2025-11-14 11:38:45 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Comb
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                if(etanol <= (gasolina * 0.7))
+                {
+                    DisplayAlert("Resultado", "Abasteça com Etanol", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Resultado", "Abasteça com Gasolina", "OK");
+                };
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Runtime.Versioning;
+
+namespace Comb
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            // Adiciona suporte apenas para Windows
+#if WINDOWS
+            builder.Logging.AddDebug();
+#endif
+#endif
+
+            return builder.Build();
+        }
+    }
+}
